979bb17bc14a5d97c82cd48605924951f6af799b
nwoik <nwokoyed@gmail.com>
2023-04-03 17:05:37 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,102 +1,14 @@
-import React, {useRef, useEffect, useState} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [videoDevice, setvideoDevice] = useState(null);
-  const [videoDevices, setVideoDevices] = useState([]);
-  const videoRef = useRef(null);
-  const [stream, setStream] = useState(null);
-  const videoDeviceId = videoDevices.length > 0 ? videoDevices[0].deviceId : undefined;
-
-  function setVideoDeviceId() {
-    navigator.mediaDevices.getUserMedia({ video: { deviceId: videoDeviceId }, audio: false })
-      .then((mediaStream) => {
-        setStream(mediaStream);
-      })
-      .catch((error) => {
-        console.error('Unable to access webcam:', error);
-      });
-  }
-  
-
-  // const [audioDevices, setAudioDevices] = useState([]);
-  // const [audioDevice, setAudioDevice] = useState(null);
-  // const audioRef = useRef(null);
-
-  // function setVideo() {
-  //   mediaDevices.then((devices) => {
-  //     const mediaDevices = devices.filter(device => device.kind === 'videoinput');
-  //     setVideoDevices(mediaDevices);
-  //     setvideoDevice(mediaDevices[0] || null);
-  //   });
-  // }
-
-  function getVideo() {
-    navigator.mediaDevices.getUserMedia({video:true}).then(stream => {
-      let video = videoRef.current;
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  }
-
-  // useEffect(() => {
-  //   getVideo();
-  // })
-
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices()
-      .then((devices) => {
-        const videoDevices = devices.filter((device) => device.kind === 'videoinput');
-        setVideoDevices(videoDevices);
-      })
-      .catch((error) => {
-        console.error('Unable to enumerate video devices:', error);
-      });
-  
-    navigator.mediaDevices.addEventListener('devicechange', (event) => {
-      navigator.mediaDevices.enumerateDevices()
-        .then((devices) => {
-          const videoDevices = devices.filter((device) => device.kind === 'videoinput');
-          setVideoDevices(videoDevices);
-        })
-        .catch((error) => {
-          console.error('Unable to enumerate video devices:', error);
-        });
-    });
-  }, []);
-
-  // for when video device is unplugged
-  useEffect(() => {
-    return () => {
-      if (stream) {
-        stream.getTracks().forEach((track) => {
-          track.stop();
-        });
-      }
-    };
-  }, [stream]);
-
   return (
-  <div className='App'>
-    <nav>
-      <a href="a">Home</a>
-    </nav>
-    <div className='camera'>
-      <video ref={videoRef}></video>
-    </div>
-    <div>
-    <select value={videoDeviceId} onChange={(event) => setVideoDeviceId(event.target.value)}>
-      {videoDevices.map((device) => (
-        <option key={device.deviceId} value={device.deviceId}>
-          {device.label}
-        </option>
-      ))}
-    </select>
-    <video ref={videoRef} autoPlay={true} />
-  </div>
-  </div>);
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import React, {useRef, useEffect, useState} from "react";
+
+function Home() {
+  const [videoDevice, setVideoDevice] = useState(null);
+  const [videoDevices, setVideoDevices] = useState([]);
+  const videoRef = useRef(null);
+  const [viewDrop, setViewDrop] = useState(false);
+  const [videoDeviceId, setVideoDeviceId] = useState(null);
+
+  function setVideo(e) {
+    e.preventDefault();
+    setVideoDeviceId(e.target.value);
+  }
+
+
+  function getCameras() {
+    navigator.mediaDevices.enumerateDevices()
+      .then((devices) => {
+        setVideoDevices(devices.filter((device) => device.kind === 'videoinput'));
+        setVideoDevice(videoDevices[0]);
+        setVideoDeviceId(videoDevices[0].deviceId)
+        setViewDrop(true);
+      })
+      .catch((error) => {
+        console.error('Unable to enumerate video devices:', error);
+      })
+  }
+
+  function getVideo() {
+    if (!videoDeviceId) {
+      getCameras();
+      return
+    }
+    navigator.mediaDevices.getUserMedia({video: {deviceId:videoDeviceId}})
+      .then(stream => {
+      setVideoDevice(videoRef.current);
+      videoDevice.srcObject = stream;
+      videoDevice.play();
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  useEffect(() => {
+    getVideo();
+  })
+
+  return (
+  <div onLoad={getVideo} className='App'>
+    <nav>
+      <a href="a">Home</a>
+    </nav>
+    <div className='camera'>
+      <video ref={videoRef}></video>
+      {viewDrop && 
+        <select name="Video Devices" onChange={setVideo} onLoad={setVideo}>
+         {videoDevices.map((v) => (<option value={v.deviceId}>{v.label}</option>))}
+      </select>}
+    </div>
+  </div>
+  )
+}
+
+export default Home;
